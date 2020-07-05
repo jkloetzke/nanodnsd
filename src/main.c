147f5902ad10bec168b4267deab8fbfb9a39c9f0
Jan Klötzke <jan@kloetzke.net>
2020-07-05 20:56:52 +0200

    main: save/restore state
    Upon receiving SIGINT/SIGTERM the state is written to the state file and
    the daemon will terminate. This state is restored when it exists.
    Respect $STATE_DIRECTORY if it has been set by systemd. Otherwise the
    file will be located based on the GNU install directory settings of
    CMake.

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -36,29 +37,60 @@
 #define EXIT_INVALID_ARGS 2
 #define EXIT_NOT_CONFIGURED 6
 
+#define STATE_FILE "nanodnsd.state"
+
+static const char *default_cfg = CMAKE_INSTALL_FULL_SYSCONFDIR "/nanodnsd.conf";
+static const char *default_state = CMAKE_INSTALL_FULL_LOCALSTATEDIR "/lib/nanodnsd/" STATE_FILE;
+
 static void help(FILE *stream, bool full)
 {
-	fprintf(stream, "usage: nanodnsd [-c CFG] [-h] [-v]\n");
+	fprintf(stream, "usage: nanodnsd [-c CFG] [-h] [-s STATE] [-v]\n");
 	if (full) {
 		fprintf(stream, "\noptions\n");
-		fprintf(stream, "    -c CFG      Read config from CFG. Default: " CMAKE_INSTALL_FULL_SYSCONFDIR "/nanodnsd.conf\n");
+		fprintf(stream, "    -c CFG      Read config from CFG. Default: %s\n", default_cfg);
 		fprintf(stream, "    -h          Show this help\n");
+		fprintf(stream, "    -s STATE    Database state. Default: %s\n", default_state);
 		fprintf(stream, "    -v          Increase log verbosity\n");
 	}
+}
+
+static int handle_sigterm(void *ctx, int sig)
+{
+	(void)sig;
+	struct poll_set *ps = ctx;
+	poll_set_interrupt(ps);
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
 	struct http_server *http_server = NULL;
 	struct dns_server *dns_server = NULL;
-	const char *cfg = CMAKE_INSTALL_FULL_SYSCONFDIR "/nanodnsd.conf";
 	int ret;
+
+#ifdef HAVE_SD_DAEMON
+	char *systemd_state = NULL;
+	{
+		char *e = getenv("STATE_DIRECTORY");
+		if (e) {
+			systemd_state = malloc(strlen(e) + strlen(STATE_FILE) + 2U);
+			if (!systemd_state)
+				return EXIT_FAILURE;
+			strcpy(systemd_state, e);
+			strcat(systemd_state, "/");
+			strcat(systemd_state, STATE_FILE);
+			default_state = systemd_state;
+		}
+	}
+#endif
 
 	/*
 	 * Parse options
 	 */
 	int opt;
-	while ((opt = getopt(argc, argv, "c:fhp:v")) != -1) {
+	const char *cfg = default_cfg;
+	const char *state = default_state;
+	while ((opt = getopt(argc, argv, "c:hs:v")) != -1) {
 		switch (opt) {
 		case 'c':
 			cfg = optarg;
@@ -66,6 +98,9 @@ int main(int argc, char **argv)
 		case 'h':
 			help(stdout, true);
 			return EXIT_SUCCESS;
+		case 's':
+			state = optarg;
+			break;
 		case 'v':
 			log_level++;
 			break;
@@ -87,6 +122,10 @@ int main(int argc, char **argv)
 	if (ret < 0)
 		return ret == -EINVAL ? EXIT_NOT_CONFIGURED : EXIT_FAILURE;
 
+	ret = db_load_state(state);
+	if (ret < 0 && ret != -ENOENT)
+		return EXIT_FAILURE;
+
 	struct poll_set *ps = poll_set_new();
 	if (!ps)
 		return EXIT_FAILURE;
@@ -106,6 +145,13 @@ int main(int argc, char **argv)
 	/*
 	 * Run
 	 */
+	signal(SIGPIPE, SIG_IGN);
+	ret = poll_set_add_signal(ps, NULL, SIGINT, handle_sigterm, ps);
+	if (ret < 0)
+		goto out;
+	ret = poll_set_add_signal(ps, NULL, SIGTERM, handle_sigterm, ps);
+	if (ret < 0)
+		goto out;
 
 	/*
 	 * Drop privileges in case we're setuid root. Or switch to other user
@@ -128,12 +174,19 @@ int main(int argc, char **argv)
 		goto out;
 	}
 
-	signal(SIGPIPE, SIG_IGN);
 	ret = poll_set_dispatch(ps);
+	if (ret >= 0)
+		ret = db_save_state(state);
+	else
+		log_err("Not saving state due to previous errors");
 
 out:
 	http_server_delete(&http_server);
 	dns_server_delete(&dns_server);
 	poll_set_delete(&ps);
+#ifdef HAVE_SD_DAEMON
+	if (systemd_state)
+		free(systemd_state);
+#endif
 	return ret >= 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
